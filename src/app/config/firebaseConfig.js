7fc41f7d5958a84a794170aba0e5213be4dae34b
Minki.Kim <mkddiction@gmail.com>
2021-03-10 13:34:11 +0900

    환경설정 소스 정리
    환경설정 소스 정리

--- src/app/config/firebaseConfig.js
+++ src/app/config/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 export const config = {
-  apiKey: "AIzaSyDaFz8jtAF8agLZqDr962s8WaEFVgWy9M4",
-  authDomain: "yeha-72ab6.firebaseapp.com",
-  databaseURL: "https://yeha-72ab6.firebaseio.com",
-  projectId: "yeha-72ab6",
-  storageBucket: "yeha-72ab6.appspot.com",
-  messagingSenderId: "850899216115",
-  appId: "1:850899216115:web:093f2783e13d80fe1a7986"
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID
 }
 
 firebase.initializeApp(config);
